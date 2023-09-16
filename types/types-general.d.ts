ea5d8bdb82518a9d34f23147ccd106918af5ce86
Aaron <aaron4uk@gmail.com>
2023-09-16 10:46:32 -0500

    infer types from schema <3

--- types/types-general.d.ts
+++ types/types-general.d.ts
@@ -1,3 +1,7 @@
 declare module 'TS_General' {
+    import { zodSchemaOverview } from '@static';
+
     type T_CardLayer = '1' | '2' | '3' | '4' | '5'
+
+    type Schema = z.infer<typeof zodSchemaOverview>;
 }
