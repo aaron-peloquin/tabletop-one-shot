3123fc7ff1bbe03ddc5695c5371ff27a95e9efa6
Aaron <aaron4uk@gmail.com>
2023-09-17 10:47:05 -0500

    fix random name generation

--- libs/helper/src/lib/useRandomName.ts
+++ libs/helper/src/lib/useRandomName.ts
@@ -10,7 +10,7 @@ export const useRandomName:T_Sig = (callback) => {
   const [nameLoading, setNameLoading] = useState(false);
   const getName = useCallback(async () => {
     setNameLoading(true);
-    fetch('/api/llm/name').then(res => res.json()).then(({message}) => {
+    fetch('/api/llm/name', {method: 'POST'}).then(res => res.json()).then(({message}) => {
       callback(message);
       setNameLoading(false);
     });
