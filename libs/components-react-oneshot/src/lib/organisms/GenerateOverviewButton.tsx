1c62c3b8bc6b50543385a3b5b7c4b9a377b6bc1e
Aaron <aaron4uk@gmail.com>
2023-10-07 10:46:16 -0500

    add status icons to overview generation

--- libs/components-react-oneshot/src/lib/organisms/GenerateOverviewButton.tsx
+++ libs/components-react-oneshot/src/lib/organisms/GenerateOverviewButton.tsx
@@ -1,7 +1,7 @@
 import { GridArea, Card, Button } from "@components-layout";
 import { useGenerateOverview } from "@helper";
 import { useContext, useCallback } from "react";
-import { globalDataContext } from "@static";
+import { ErrorIcon, IdleIcon, LoadingIcon, SuccessIcon, globalDataContext } from "@static";
 import React from "react";
 
 type T_Props = {
@@ -9,7 +9,7 @@ type T_Props = {
 };
 
 export const GenerateOverviewButton: React.FC<T_Props> = ({gridName}) => {
-  const {overview, name, clearOverview} = useContext(globalDataContext);
+  const {overview, name, clearOverview, overviewError} = useContext(globalDataContext);
   const {generateOverview, overviewLoading} = useGenerateOverview();
   
   const handleGenerateOverview = useCallback(() => {
@@ -21,7 +21,14 @@ export const GenerateOverviewButton: React.FC<T_Props> = ({gridName}) => {
   return <GridArea className='full-width' name={gridName}>
     {overview
       ? <Button style={{fontWeight:'bold', color: '#694834'}} text="Clear Data" onClick={clearOverview} />
-      : <Button style={{fontWeight:'bold', color: '#586'}} text="Generate Session Overview" disabled={disabled} onClick={handleGenerateOverview} />}
+      : <Button style={{fontWeight:'bold', color: '#586'}} text="Generate Session" disabled={disabled} onClick={handleGenerateOverview} />}
+    {overviewLoading
+      ? <LoadingIcon />
+      :overviewError
+        ?<ErrorIcon />
+        :overview
+          ?<SuccessIcon />
+          :<IdleIcon />}
   </GridArea>;
 
 };
