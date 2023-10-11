ec365521548cf5b056dbe073889fdd7100ecb9dc
Aaron <aaron4uk@gmail.com>
2023-10-10 20:25:41 -0500

    add resolutions

--- libs/components-react-oneshot/src/lib/organisms/OverviewLayout.tsx
+++ libs/components-react-oneshot/src/lib/organisms/OverviewLayout.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { GridArea, Card } from "@components-layout";
 import { useContext } from "react";
 import { globalDataContext } from "@static";
@@ -11,12 +12,17 @@ export const OverviewLayout: React.FC<T_Props> = ({gridName}) => {
 
   return <GridArea name={gridName} className="full-width">
     {overviewError && <Card layer="1" heading="Error">{overviewError}</Card>}
-    {overview && 
+    {overview?.description && 
       <Card layer="2" heading="Overview Output">
         {overview?.description}
         {overview?.hooks?.length && <Card layer="2" heading="Adventure Hooks">
           <Card layer="3">
             <ul>{overview?.hooks?.map(hook => <li>{hook}</li>)}</ul>
+          </Card>
+        </Card>}
+        {overview?.resolutions?.length && <Card layer="2" heading="Possible Resolutions">
+          <Card layer="3">
+            <ul>{overview?.resolutions?.map(resolution => <li>{resolution}</li>)}</ul>
           </Card>
         </Card>}
       </Card>
