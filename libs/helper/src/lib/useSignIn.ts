c9853f2e8618269bb3c91aedf700852c2e57d7f7
Aaron <aaron4uk@gmail.com>
2024-06-27 19:57:00 -0500

    optional chain

--- libs/helper/src/lib/useSignIn.ts
+++ libs/helper/src/lib/useSignIn.ts
@@ -7,7 +7,7 @@ import { useSearchParams } from 'next/navigation';
 
 export const useSignIn = () => {
   const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || "/";
+  const callbackUrl = searchParams?.get("callbackUrl") || "/";
   const handleSignIn = useCallback(() => {
     return signIn("google", { callbackUrl });
   }, [callbackUrl]);
